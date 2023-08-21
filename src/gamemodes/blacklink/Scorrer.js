d396a94a85fc174b10631c30eb2032aad0724354
thijsvredenbregt <thijs@vredenbregt.net>
2023-08-21 08:00:34 +0200

    added black link gamemode

--- src/gamemodes/blacklink/Scorrer.js
+++ src/gamemodes/blacklink/Scorrer.js
@@ -5,7 +5,7 @@ export default Props => {
 	return (
 		<div className="row-container" style={{backgroundColor:Props.color}}>
 			<div className="checkbox-container" style={{backgroundColor: Props.darkColor}}>
-				{Props.items.map((item, index) => <ScorrerCheckbox key={index} lightColor={Props.lightColor} index={index} value={item.value} row={Props.row} checked={item.checked} disabled={item.disabled} onClickHandler={Props.onClickHandler} />)}
+				{Props.items.map((item, index) => <ScorrerCheckbox key={index} id={item.id} link={item.link} lightColor={Props.lightColor} index={index} value={item.value} row={Props.row} checked={item.checked} disabled={item.disabled} onClickHandler={Props.onClickHandler} />)}
 			</div>
 			<div className={Props.items[Props.items.length-1].checked ? "locked" : "unlocked"}>&nbsp;</div>
 		</div>
--- src/gamemodes/blacklink/ScorrerCheckbox.js
+++ src/gamemodes/blacklink/ScorrerCheckbox.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import Xarrow from "react-xarrows";
 
 export default Props => {
   return (
-    <div className={Props.disabled ? Props.checked ? "checkbox-disabled checkbox-checked" : "checkbox-disabled" : Props.checked ? "checkbox-checked" : null} style={{backgroundColor:Props.lightColor}} data-index={Props.index} data-row={Props.row} data-checked={Props.checked} data-disabled={Props.disabled} data-value={Props.value} onClick={Props.onClickHandler}>{Props.value}</div>
-  );
+      <>
+        <div className={Props.disabled ? Props.checked ? "checkbox-disabled checkbox-checked" : "checkbox-disabled" : Props.checked ? "checkbox-checked" : null} style={{backgroundColor:Props.lightColor}} data-index={Props.index} data-row={Props.row} data-checked={Props.checked} data-disabled={Props.disabled} data-value={Props.value} onClick={Props.onClickHandler}>
+            <div data-index={Props.index} data-row={Props.row} data-checked={Props.checked} data-disabled={Props.disabled} data-value={Props.value} onClick={Props.onClickHandler} id={Props.id} className={Props.link === undefined ? "":"link"}>
+                {Props.value}
+            </div>
+        </div>
+        {Props.link !== undefined ? <Xarrow start={Props.id} end={Props.link} color={"black"} showHead={false} />:<></>}
+      </>);
 };
